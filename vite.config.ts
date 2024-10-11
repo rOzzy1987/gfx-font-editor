969d46df4b446527e861af0aa3093c0000b4b98d
rOzzy1987 <rozovits.mihaly@gmail.com>
2024-10-12 01:43:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/gfx-font-editor/',
     plugins: [vue(), vueDevTools()],
     resolve: {
         alias: {
